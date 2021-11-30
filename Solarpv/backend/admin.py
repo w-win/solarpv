a61b6958861d3c4d9f69057ca828777d0b990c84
jeffreyhannah <70003821+jeffreyhannah@users.noreply.github.com>
2021-11-30 16:33:26 -0700

    Add files via upload

--- Solarpv/backend/admin.py
+++ Solarpv/backend/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import service,product,certificate
 
+admin.site.register(service)
+admin.site.register(product)
+admin.site.register(certificate)
 # Register your models here.
